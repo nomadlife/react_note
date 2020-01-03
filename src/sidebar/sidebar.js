b5ce81a9db030e5cc4b8a021546eafb1e4ab5ff3
nomadlife_15w <joonwoo3816@gmail.com>
2020-01-03 20:35:09 +0900

    add sidebar

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import styles from './styles';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { removeHTMLTags } from '../helpers';
-import SidebarItemComponent from '../sidebaritem/sidebaritem'
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles";
+import ListItem from "@material-ui/core/ListItem";
+import { Divider, Button, ButtonGroup } from "@material-ui/core";
+import ListItemText from "@material-ui/core/ListItemText";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { removeHTMLTags } from "../helpers";
+import SidebarItemComponent from "../sidebaritem/sidebaritem";
 
 class SidebarComponent extends React.Component {
-    constructor(){
-        super();
-    }
+  constructor() {
+    super();
+    this.state = {
+      addingNote: false,
+      title: null
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                hello from the sidebar
-            </div>
-        )
-    }
+  render() {
+    const { notes, classes, selectedNoteIndex } = this.props;
+
+    return (
+      <div className={classes.sidebarContainer}>
+        <Button
+          onClick={this.newNoteBtnClick}
+          className={classes.newNoteBtnClick}
+        >
+          New Note
+        </Button>
+        {this.state.addingNote ? (
+          <div>
+            <input
+              className={classes.newNoteInput}
+              placeholder="Enter note title"
+              onKeyUp={e => this.updateTitle(e.target.value)}
+            ></input>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+  newNoteBtnClick = () => {
+    this.setState({ title:null,addingNote: !this.state.addingNote})
+  };
+  updateTitle = txt => {
+    console.log("Here it is: ", txt);
+  };
 }
 
 export default withStyles(styles)(SidebarComponent);
