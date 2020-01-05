ffb16b8e9ed25e92cb985737505c5be0a6d8e3c6
nomadlife_mac <>
2020-01-05 19:29:49 +0900

    update new note funciton

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -77,7 +77,8 @@ class SidebarComponent extends React.Component {
   };
 
   newNote = () => {
-      console.log(this.state);
+      this.props.newNote(this.state.title);
+      this.setState({ title:null, addingNote: false})
   };
 
   selectNote = (n, i) => this.props.selectNote(n, i);
